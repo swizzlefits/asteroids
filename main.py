d8475d7de569011a9ca2e7231c54be352830fc78
swizzlefits <kyle.g.barker@gmail.com>
2025-01-17 04:10:58 -0800

    Added circleshape

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill(BLACK)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
