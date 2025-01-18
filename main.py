d98bbe44e0788b2b0fea5c404dc1e2dc2eb1df0d
swizzlefits <kyle.g.barker@gmail.com>
2025-01-17 16:59:30 -0800

    player movement implemented

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
 
 def main():
     pygame.init()
@@ -14,13 +20,27 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill(BLACK)
+
+        updatable.update(dt)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
+    
+    
 
 if __name__ == "__main__":
     main()
